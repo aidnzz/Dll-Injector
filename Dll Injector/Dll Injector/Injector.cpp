58fda1c04ee9319baef8deb7f068661559f3d863
Mark W <mangowalk04@gmail.com>
2018-02-27 23:56:02 +0000

    Update Injector.cpp
    Improved readability

--- Dll Injector/Dll Injector/Injector.cpp
+++ Dll Injector/Dll Injector/Injector.cpp
@@ -7,32 +7,37 @@
 
 void Injector::inject(const wchar_t* dllPath) const
 {
-	const size_t size = wsizeof(dllPath);
+	wchar_t buffer[MAX_PATH + 1] = {  };
+
+	GetFullPathNameW(dllPath, MAX_PATH, buffer, nullptr);
+	const size_t nSize = wsizeof(buffer);
 
 	LPVOID loadLibAddr = GetProcAddress(GetModuleHandleA("Kernel32.dll"), "LoadLibraryW"); // Gets address of loadlibraryw for unicode support when loading dll path
-	LPVOID pDllPath = VirtualAllocEx(hProcess_, nullptr, size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+	LPVOID lpDllPath = VirtualAllocEx(hProcess_, nullptr, nSize, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
 
-	if (pDllPath == nullptr)
+	if (lpDllPath == nullptr)
 		throw std::runtime_error("Error reserving process memory!");
 
-	if (!writeBuffer((DWORD)pDllPath, dllPath, size))
+	if (!writeBuffer((DWORD)lpDllPath, buffer, nSize))
 	{
-		VirtualFreeEx(hProcess_, pDllPath, 0, MEM_RELEASE);
+		VirtualFreeEx(hProcess_, lpDllPath, 0, MEM_RELEASE);
 		throw std::runtime_error("Error writing dll path to process");
 	}
 
-	HANDLE remoteThread = CreateRemoteThread(hProcess_, nullptr, size, (LPTHREAD_START_ROUTINE)loadLibAddr, pDllPath, NULL, nullptr);
-	if (remoteThread == nullptr)
+	HANDLE hRemoteThread = CreateRemoteThread(hProcess_, nullptr, nSize, (LPTHREAD_START_ROUTINE)loadLibAddr, lpDllPath, NULL, nullptr);
+	if (hRemoteThread == nullptr)
 	{
-		VirtualFreeEx(hProcess_, pDllPath, 0, MEM_RELEASE);
+		VirtualFreeEx(hProcess_, lpDllPath, 0, MEM_RELEASE);
 		throw std::runtime_error("Error creating remote thread!");
 	}
 
-	std::cout << "[*] Waiting for dll to finish executing!" << std::endl;
+	CloseHandle(hRemoteThread);
 
-	WaitForSingleObject(remoteThread, INFINITE);
-	CloseHandle(remoteThread);
-
-	if (!VirtualFreeEx(hProcess_, pDllPath, 0, MEM_RELEASE))
+	if (!VirtualFreeEx(hProcess_,
+		lpDllPath,
+		0,
+		MEM_RELEASE))
+	{
 		throw std::runtime_error("Error freeing memory from process!");
+	}
 }
