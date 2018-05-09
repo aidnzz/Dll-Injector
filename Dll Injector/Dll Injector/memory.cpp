08ce746dfe41aced85708c685143cb70d572a17e
Mark W <mangowalk04@gmail.com>
2018-05-09 21:04:57 +0100

    Update and rename Memory.cpp to memory.cpp
    Changed file name and made it simpler

---
+++ Dll Injector/Dll Injector/memory.cpp
@@ -0,0 +1,82 @@
+#include "memory.h"
+
+#include <TlHelp32.h>
+
+void Memory::attach(const wchar_t* szProcessName, DWORD dwAccessRights)
+{
+	PROCESSENTRY32W processEntry;
+	HANDLE hProcess = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
+
+	if (hProcess == INVALID_HANDLE_VALUE)
+		throw std::runtime_error("Error: invalid process entry handle for snapshots!");
+
+	processEntry.dwSize = sizeof(PROCESSENTRY32W);
+
+	if (!Process32FirstW(hProcess, &processEntry))
+	{
+		CloseHandle(hProcess);
+		throw std::runtime_error("Error: could not enumerate through processes!");
+	}
+
+	m_szProcessName = szProcessName; // set process name
+
+	do
+	{
+		if (lstrcmpW(szProcessName, processEntry.szExeFile) == 0)
+		{
+			CloseHandle(hProcess);
+			m_hProcess = OpenProcess(dwAccessRights, false, processEntry.th32ProcessID);
+
+			if (m_hProcess == nullptr)
+				throw std::runtime_error("Error: could not open process!");
+
+			m_pId = processEntry.th32ProcessID;
+			return;
+		}
+	} while (Process32NextW(hProcess, &processEntry));
+
+	CloseHandle(hProcess);
+	throw std::runtime_error("Error: could not find process!");
+}
+
+Module Memory::getModuleInfo(const wchar_t* szModuleName)
+{
+	MODULEENTRY32W moduleEntry;
+	HANDLE hModule = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, m_pId);
+
+	if (hModule == INVALID_HANDLE_VALUE)
+		throw std::runtime_error("Error: invalid Handle value!");
+
+	moduleEntry.dwSize = sizeof(moduleEntry);
+
+	if (!Module32FirstW(hModule, &moduleEntry))
+	{
+		CloseHandle(hModule);
+		throw std::runtime_error("Error: could not enumerate modules through process!");
+	}
+
+	if (szModuleName == nullptr)
+		szModuleName = m_szProcessName;
+
+	do
+	{
+		if (lstrcmpW(moduleEntry.szModule, szModuleName) == 0)
+		{
+			CloseHandle(hModule);
+			return { reinterpret_cast<uintptr_t>(moduleEntry.modBaseAddr), moduleEntry.dwSize };
+		}
+	} while (Module32NextW(hModule, &moduleEntry));
+
+	CloseHandle(hModule);
+	throw std::runtime_error("Error: could not find module!");
+}
+
+bool Memory::writeBuffer(uintptr_t addr, LPCVOID buffer, uint32_t nSize) const
+{
+	return WriteProcessMemory(m_hProcess, reinterpret_cast<LPVOID>(addr), buffer, nSize, nullptr);
+}
+
+std::wostream& operator<<(std::wostream& stream, const Memory& other)
+{
+	return stream << "Process name: " << other.m_szProcessName << " process identifier: " << other.m_pId;
+}
