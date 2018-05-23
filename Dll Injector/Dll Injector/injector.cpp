307f31dcff072eb094a6935199834f91501416a1
Mark W <mangowalk04@gmail.com>
2018-05-23 16:09:16 +0100

    Update injector.cpp

--- Dll Injector/Dll Injector/injector.cpp
+++ Dll Injector/Dll Injector/injector.cpp
@@ -6,41 +6,41 @@
 
 void Injector::inject(const wchar_t* dllPath) const
 {
-	WCHAR szFullPath[261]; 
-	UINT nSize = sizeof(szFullPath); // Get size of buffer in bytes
+    WCHAR szFullPath[261]; 
+    UINT nSize = sizeof(szFullPath); // Get size of buffer in bytes
 
-	ZeroMemory(szFullPath, nSize);
-	GetFullPathNameW(dllPath, 260, szFullPath, nullptr);
+    ZeroMemory(szFullPath, nSize);
+    GetFullPathNameW(dllPath, 260, szFullPath, nullptr);
 
-	HMODULE hModule = GetModuleHandleA(KERNEL32);
-	LPVOID addr = GetProcAddress(hModule, LOADLIBRARYW); 
+    HMODULE hModule = GetModuleHandleA(KERNEL32);
+    LPVOID addr = GetProcAddress(hModule, LOADLIBRARYW); 
 
-	LPVOID lpParam = VirtualAllocEx(m_hProcess, nullptr, nSize, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE); // Allocate Memory for buffer
+    LPVOID lpParam = VirtualAllocEx(m_hProcess, nullptr, nSize, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE); // Allocate Memory for buffer
 
-	if (lpParam == nullptr)
-		throw std::runtime_error("Error: could not reserving memory in process!");
+    if (lpParam == nullptr)
+        throw std::runtime_error("Error: could not reserving memory in process!");
 
-	std::cout << "\n[+] Allocated " << nSize << " bytes of memory in process" << '\n';
+    std::cout << "\n[+] Allocated " << nSize << " bytes of memory in process" << '\n';
 
-	if (!writeBuffer(reinterpret_cast<uintptr_t>(lpParam), szFullPath, nSize))
-	{
-		VirtualFreeEx(m_hProcess, lpParam, 0, MEM_RELEASE);
-		throw std::runtime_error("Error: could not write dll path to process");
-	}
+    if (!writeBuffer(reinterpret_cast<uintptr_t>(lpParam), szFullPath, nSize))
+    {
+        VirtualFreeEx(m_hProcess, addr, 0, MEM_RELEASE);
+        throw std::runtime_error("Error: could not write dll path to process");
+    }
 
-	std::cout << "[+] Wrote DLL path to " << std::hex << std::showbase << reinterpret_cast<uintptr_t>(addr) << " in process memory" << '\n';
+    std::cout << "[+] Wrote DLL path to " << std::hex << reinterpret_cast<uintptr_t>(addr) << " in process memory" << '\n';
 
-	HANDLE hThread = CreateRemoteThread(m_hProcess, nullptr, 0, static_cast<LPTHREAD_START_ROUTINE>(addr), lpParam, 0, nullptr);
+    HANDLE hThread = CreateRemoteThread(m_hProcess, nullptr, 0, static_cast<LPTHREAD_START_ROUTINE>(addr), lpParam, 0, nullptr);
 
-	if (hThread == nullptr)
-	{
-		VirtualFreeEx(m_hProcess, lpParam, 0, MEM_RELEASE);
-		throw std::runtime_error("Error: could not create remote thread!");
-	}
+    if (hThread == nullptr)
+    {
+        VirtualFreeEx(m_hProcess, addr, 0, MEM_RELEASE);
+        throw std::runtime_error("Error: could not create remote thread!");
+    }
 
-	std::cout << "[+] Executing DLL in target process! " << '\n';
-	WaitForSingleObject(hThread, INFINITE);
+    std::cout << "[+] Executing DLL in target process! " << '\n';
+    WaitForSingleObject(hThread, INFINITE);
 
-	VirtualFreeEx(m_hProcess, lpParam, 0, MEM_RELEASE);
-	CloseHandle(hThread);
+    VirtualFreeEx(m_hProcess, addr, 0, MEM_RELEASE);
+    CloseHandle(hThread);
 }
